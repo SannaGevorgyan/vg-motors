4298a76874fb582f4918629a5e1ec6d9c0d1d3fa
Susanna Gevorgyan <susanna.gevorgyan@inconceptlabs.com>
2025-02-08 23:17:56 +0400

    init

---
+++ app/components/Navbar.js
@@ -0,0 +1,24 @@
+import Link from "next/link";
+import Image from "next/image";
+
+export default function Navbar() {
+  return (
+    <nav className="py-4 px-8 shadow-md">
+      <div className="container mx-auto flex justify-between items-center">
+        <Link href="/" className="text-blue-700 text-2xl font-bold">
+          <Image
+            src="/logo.jpg"
+            width={160}
+            height={48}
+            alt=""
+          />
+        </Link>
+        <div className="space-x-6 hidden md:flex">
+          <Link href="/" className="text-white hover:text-gray-200">Home</Link>
+          <Link href="/about" className="text-white hover:text-gray-200">About</Link>
+          <Link href="/contact" className="text-white hover:text-gray-200">Contact</Link>
+        </div>
+      </div>
+    </nav>
+  );
+}
